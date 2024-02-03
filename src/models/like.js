fc2bfc8664e266c3d1167ced26b8bc1d584494d6
nishant <nishant.20235@knit.ac.in>
2024-02-03 12:45:24 +0530

    added like model and basic user model

---
+++ src/models/like.js
@@ -0,0 +1,22 @@
+import mongoose from '.mongoose';
+
+const likeSchema = mongoose.Schema({
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Tweet','Comment'] 
+    },
+    likeable: {
+        type: String,
+        required: true,
+        refPath: 'onModel'
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+}, {timestamps: true});
+
+const Like = mongoose.model('Like',likeSchema);
+export default Like;
