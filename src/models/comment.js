2b941e3915f587d3155f286e9e42a46df7bb6197
nishant <nishant.20235@knit.ac.in>
2024-02-24 18:10:33 +0530

    updated comment model with refpath

--- src/models/comment.js
+++ src/models/comment.js
@@ -6,8 +6,20 @@ const commentSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    useremail: {
-      type: String
+    userId: {
+      type: mongoose.Schema.Types.ObjectId(),
+      ref: 'User',
+      required: true
+    },
+    onModel: {
+      type: String,
+      required: true,
+      enum: ['Tweet','Comment']
+    },
+    commentable: {
+      type: mongoose.Schema.Types.ObjectId(),
+      reqiured: true,
+      refPath: 'onModel',
     }
 }, {timestamps: true});
 
