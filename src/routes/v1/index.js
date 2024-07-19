a594e9a7e16c7944a7bdf9eedbbe37e670c702ed
nishant <nishant.20235@knit.ac.in>
2024-07-19 18:13:24 +0530

    added city controller

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,19 +1,18 @@
 // const express = require('express');
-import express from 'express';
+import express from "express";
 const router = express.Router();
 // const {createTweet} = require('../../controllers/tweet-controller');
-import { createTweet } from '../../controllers/tweet-controller.js';
-import { toggleLike } from '../../controllers/like-controller.js';
-import { createComment } from '../../controllers/comment-controller.js';
-import { signup, login } from '../../controllers/auth-controller.js';
-import { authenticate } from '../../middlewares/authenticate.js';
+import { createTweet } from "../../controllers/tweet-controller.js";
+import { toggleLike } from "../../controllers/like-controller.js";
+import { createComment } from "../../controllers/comment-controller.js";
+import { signup, login } from "../../controllers/auth-controller.js";
+import { authenticate } from "../../middlewares/authenticate.js";
 
-
-router.post('/tweets',authenticate,createTweet);
-router.post('/likes/toggle',toggleLike);
-router.post('/comments',createComment);
-router.post('/signup',signup);
-router.post('/login',login);
+router.post("/tweets", authenticate, createTweet);
+router.post("/likes/toggle", toggleLike);
+router.post("/comments", authenticate, createComment);
+router.post("/signup", signup);
+router.post("/login", login);
 
 // module.exports = router;
 export default router;
